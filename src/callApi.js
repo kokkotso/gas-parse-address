beaf38471f6ebff476c52e0b5a2d40659a71ddb6
Kokko Tso <kokkotso@gmail.com>
2021-05-21 01:42:31 -0400

    feature: make call to Google Places API

--- src/callApi.js
+++ src/callApi.js
@@ -1,5 +1,13 @@
-function callApi(address) {
-    console.log(address);
-}
+import secrets from '../src/secrets.js';
 
-module.exports = callApi;
+// const axios = require('axios');
+
+const apiKey = secrets.apiKey;
+
+export default function callApi(address) {
+    const response = UrlFetchApp.fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${apiKey}&input=${address}&inputtype=textquery`, {contentType: "application/json"});
+    const data = JSON.parse(response.getContentText());
+    console.log(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${apiKey}&input=${address}&inputtype=textquery`);
+    console.log(response.getResponseCode());
+    console.log(data);
+}
