133cbee6a8d6b5cd6d4d349ce4834b6defb8dfdd
Kokko Tso <kokkotso@gmail.com>
2021-05-22 20:08:32 -0400

    refactor: return formatted location object from callApi

--- src/callApi.js
+++ src/callApi.js
@@ -29,15 +29,20 @@ export function callApi(address) {
 
         const geoData = JSON.parse(geoResponse.getContentText()).results[0];
         console.log(geoData);
-        const geoFormatted = geoData.formatted_address;
-        const geoComponents = geoData.address_components;
-        const geoCoordinates = geoData.geometry.location;
 
-        console.log(geoData);
-        console.log(geoFormatted);
-        console.log(geoComponents);
-        console.log(geoCoordinates);
+        const locObj = {
+            formattedAddress: geoData.formatted_address || undefined,
+            lat: geoData.geometry.location.lat || undefined,
+            long: geoData.geometry.location.lng || undefined,
+            code: geoData.address_components.find(component => component.types.includes("postal_code")).long_name || undefined,
+            country: geoData.address_components.find(component => component.types.includes("country")).long_name || undefined,
+            state: geoData.address_components.find(component => component.types.includes("administrative_area_level_1" || "administrative_area_level_2" || "administrative_area_level_3" || "administrative_area_level_4" || "administrative_area_level_5")).long_name || undefined,
+            city: geoData.address_components.find(component => component.types.includes("locality")).long_name || undefined,
+            street: geoData.formatted_address.slice(0, geoData.formatted_address.indexOf(","))
+        }
+
+        return locObj;
     }
 
-    fetchInfo();
+    return fetchInfo();
 }
